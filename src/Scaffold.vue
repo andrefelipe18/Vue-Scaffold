d9273db8a6347e21d4193267e03cdff2a5dabb7d
André Domingues <dominguesstroppa@gmail.com>
2023-06-29 17:56:41 -0300

    Criação do projeto, adicionar tailwind
    Tailwind Prefix: Para utilizar as classes do tailwind é necessário utilizar o prefix tw-

---
+++ src/Scaffold.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'; //Nao precisa de defineProps
+  
+
+</script>
+
+<template>
+    <h1>Scaffold</h1>
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.vue', '/src/*.vue', '/public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'tw-',
+}
+
